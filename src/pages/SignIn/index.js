fa5560f34fe02b5ec96d4eca791fbca6879cc1e9
rafasnarf <snarfelcv@gmail.com>
2020-03-12 21:54:10 -0300

    Configurando Redux nas paginas de login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSumbit(data) {
+    console.tron.log(data);
+    // dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber Web" />
+
+      <Form schema={schema} onSubmit={handleSumbit}>
+        <Input name="email " type="email" placeholder="Seu email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SingIn from '../pages/SingIn';
-import SingUP from '../pages/SingUp';
+import SignIn from '../pages/SignIn';
+import SignUP from '../pages/SignUp';
 import Profile from '../pages/Profile';
 import Dashbord from '../pages/Dashbord';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={SingIn} />
-      <Route path="/register" component={SingUP} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUP} />
 
       <Route path="/dashboard" component={Dashbord} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
